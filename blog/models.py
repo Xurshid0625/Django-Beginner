5692dbfac3e267d0217fcb1fc9290740265bc178
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-01-20 22:49:13 +0500

    add post rating comment to model

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Tag(models.Model):
@@ -14,4 +14,40 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name="Post title", max_length=255)
+    body = models.TextField(verbose_name="Post Body")
+    author = models.CharField(
+        verbose_name="Post Author", default="Admin", max_length=100
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="categories"
+    )
+    tag = models.ManyToManyField(Tag, on_delete=models.PROTECT, related_name="tags")
+    views = models.PositiveIntegerField(default=0)
+    publish_date = models.DateTimeField(verbose_name="Publish time", auto_now_add=True)
+    published = models.BooleanField(default=True)
+    on_top = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Comment(models.Model):
+    author = models.CharField(verbose_name="Post Author", max_length=100, blank=False)
+    comment = models.TextField(verbose_name="Comment")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return str(self.author)
+
+
+class Rating(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="ratings")
+    value = models.PositiveSmallIntegerField(verbose_name="Post Rating", default=0)
+
+    def __str__(self):
+        return str(self.value)
