387b90a116060865f6fda25755f55b867fc55417
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-01-25 14:43:38 +0500

    add js message and message notif

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Category, Post, Comment, Rating
 
 from .utils import check_read_articles
